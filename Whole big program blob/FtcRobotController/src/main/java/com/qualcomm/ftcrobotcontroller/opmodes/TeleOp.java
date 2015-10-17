ebe6a768af65928121a8849559366f9f88f4c208
Tater Talker <sharkyxd7@gmail.com>
2015-10-17 14:46:51 -0400

    Added folder for autonomous methods

--- Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
+++ Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.ftcrobotcontroller.opmodes.mainDriving;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 
 /**
  * TeleOp Mode
---
+++ Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/accelerometerTest.java
@@ -0,0 +1,7 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by August on 10/16/2015.
+ */
+public class accelerometerTest {
+}
