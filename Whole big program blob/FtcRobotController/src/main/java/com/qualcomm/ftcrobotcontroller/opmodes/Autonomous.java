5957b3433d674cba6d4ecd15a07ca040dcd390d3
davidhnguyen2000 <davidhnguyen2000@gmail.com>
2015-11-15 15:56:27 -0500

    MADE AUTONOMOUS WORK
    Fixed a bunch of problems with the autonomous

--- Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
+++ Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
@@ -14,9 +14,11 @@ public class Autonomous extends OpMode {
     DcMotor BR;
     DcMotor BL;
     DcMotor FR;
+    DcMotor collector;
     Servo climberDumper;
 
     private int v_state = 0;
+    private boolean encoders_have_reset=false;
 
     //Map the motors.
     public void init() {
@@ -26,6 +28,7 @@ public class Autonomous extends OpMode {
         FL = hardwareMap.dcMotor.get("FL");
         BR = hardwareMap.dcMotor.get("BR");
         BL = hardwareMap.dcMotor.get("BL");
+        collector = hardwareMap.dcMotor.get("colmot");
     }
 
     //Start the program and reset the encoders.
@@ -49,45 +52,99 @@ public class Autonomous extends OpMode {
 
             case 0:
                 reset_drive_encoders();
-                run_using_encoders();
+                //run_using_encoders();
                 v_state++;
                 break;
 
             case 1:
-                turn(310, 0.2);
-                telemetry.addData("rightTicks", "" + FL.getCurrentPosition());
-                telemetry.addData("leftTicks", "" + FR.getCurrentPosition());
+                if(encoders_have_reset || have_drive_encoders_reset()) {
+                   encoders_have_reset= true;
+                    drive_with_collector(2000, 1);
+                }
                 break;
-
+                
             case 2:
-                drive(5000,1);
+                if(encoders_have_reset || have_drive_encoders_reset()) {
+                    encoders_have_reset=true;
+                    turn(45, -1);
+                    telemetry.addData("rightTicks", "" + FL.getCurrentPosition());
+                    telemetry.addData("leftTicks", "" + FR.getCurrentPosition());
+                }
                 break;
-            case 3:
-                climberDumper.setPosition(0);
-            default:
+                
 
+            case 3:
+                if(encoders_have_reset || have_drive_encoders_reset()) {
+                    encoders_have_reset=true;
+                    drive_with_collector(6000, 1);
+                }
                 break;
+            case 4:
+                if(encoders_have_reset || have_drive_encoders_reset()) {
+                    encoders_have_reset=true;
+                    turn(45, -1);
+                    telemetry.addData("rightTicks", "" + FL.getCurrentPosition());
+                    telemetry.addData("leftTicks", "" + FR.getCurrentPosition());
+                }
+                break;
+            case 5:
+                if(encoders_have_reset || have_drive_encoders_reset()) {
+                    encoders_have_reset=true;
+                    drive(450, 0.5);
+                }
+                break;
+            case 6:
+                if(encoders_have_reset || have_drive_encoders_reset()) {
+                    encoders_have_reset=true;
+                    climberDumper.setPosition(0);
+                }
+                break;
+            default:
         }
-        telemetry.addData("Text", "State: " + v_state);
+        //telemetry.addData("Text", "State: " + v_state + " " + have_drive_encoders_reset()+ " " + encoders_have_reset);
+
+        telemetry.addData("Text", "State: " + v_state + " " + FR.getCurrentPosition() + " " + BR.getCurrentPosition()+ " " + FL.getCurrentPosition() + " " + BL.getCurrentPosition());
     }
 
-    //The drive function.
+    //The drive with collector function.
 
 
-    void drive(float distance, float speed) {
+    void drive_with_collector(float distance, double speed) {
     // Start the drive wheel motors at full power
 
-        if (hasLeftReached(distance) && hasRightReached(distance)) {
+        if (hasLeftReached(distance) || hasRightReached(distance)) {
             setLeftPower(0);
             setRightPower(0);
+            collector.setPower(0);
+            reset_drive_encoders();
             v_state++;
         }
 
         else {
+            run_using_encoders();
+            setLeftPower(speed);
+            setRightPower(speed);
+            collector.setPower(1);
+        }
+    }
+
+    void drive(float distance, double speed) {
+        // Start the drive wheel motors at full power
+
+        if (hasLeftReached(distance) || hasRightReached(distance)) {
+            setLeftPower(0);
+            setRightPower(0);
+            reset_drive_encoders();
+            v_state++;
+        }
+
+        else {
+            run_using_encoders();
             setLeftPower(speed);
             setRightPower(speed);
         }
     }
+
 
     void reset_drive_encoders() {
 
@@ -95,6 +152,7 @@ public class Autonomous extends OpMode {
         FR.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         BL.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         BR.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        encoders_have_reset=false;
     }
 
     void run_using_encoders() {
@@ -137,7 +195,7 @@ public class Autonomous extends OpMode {
         BR.setPower(power);
     }
 
-    double clip(double variable, double min, double max){
+    double clip(double variable, double min, double max) {
         if(variable<min)
             variable=min;
 
@@ -153,16 +211,18 @@ public class Autonomous extends OpMode {
     }
 
     void turn(int degrees, double power){
-        if(hasLeftReached(degrees*10)&&hasRightReached(degrees*10)){
-            setLeftPower(0);
-            setRightPower(0);
-            reset_drive_encoders();
-            run_using_encoders();
-            v_state++;
-        }
-        else{
+        if(hasLeftReached(degrees*21)||hasRightReached(degrees * 21))
+        {
+                setLeftPower(0);
+                setRightPower(0);
+                reset_drive_encoders();
+                v_state++;
+            }
+            else{
+                run_using_encoders();
             setLeftPower(power);
             setRightPower(-power);
+            }
         }
-    }
+
 }
