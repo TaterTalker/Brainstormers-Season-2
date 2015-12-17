eb2a3565fbb56ccfcadfadce20b9ef1f9b03e44a
Tater Talker <sharkyxd7@gmail.com>
2015-12-17 18:15:48 -0500

    made (partially) working gyrotun function in turntest

--- Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/turnTest.java
+++ Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/turnTest.java
@@ -1,107 +1,94 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 
 /**
  * Created by August on 12/5/2015.
  */
-public class turnTest extends OpMode {
+public class turnTest extends LinearOpMode {
     DcMotor FL;
     DcMotor BR;
     DcMotor BL;
     DcMotor FR;
-    int loopCount=0;
-    boolean turnComplete=false;
+    int loopCount = 0;
+    boolean turnComplete = false;
     GyroSensor gyroSensor;
-    int i=0;
+    int i = 0;
     int lastgyro;
 
+
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
         FR = hardwareMap.dcMotor.get("FR");
         FL = hardwareMap.dcMotor.get("FL");
         BR = hardwareMap.dcMotor.get("BR");
         BL = hardwareMap.dcMotor.get("BL");
         gyroSensor = hardwareMap.gyroSensor.get("G1");
         gyroSensor.calibrate();
-        lastgyro=0;
-    }
-
-    @Override
-    public void loop() {
-        switch (i){
-            case 0:
-                resetGyro();
-                break;
-            case 1:
-                turnWithGyro(90);
-                break;
-            case 2:
-                pause(100);
-                break;
-            case 3:
-                turnWithGyro(90);
-                break;
-            default:
-                break;
-        }
+        sleep(5000);
+        waitForStart();
+        turnWithGyro(90);
+        sleep(1000);
+        turnWithGyro(180);
+        sleep(1000);
+        turnWithGyro(-270);
     }
 
 
     void resetGyro() {
-        telemetry.addData("heading: ", "" + gyroSensor.getHeading());
-        gyroSensor.calibrate();
-        if (gyroSensor.getHeading() == 0) {
-            i++;
-
-            if (gyroSensor.getHeading() != 0) {
-                gyroSensor.calibrate();
-            } else {
-                pause(450);
-
-            }
+        while(heading()!=0) {
+            lastgyro = gyroSensor.getHeading();
         }
     }
-    void turnWithGyro(int degrees){
-        telemetry.addData("heading: ", "" + gyroSensor.getHeading());
-        degrees=degrees-10;
-        if (degrees<0){
-            degrees+=360;
-        }
-            degrees-=7;
 
-        int curDegs = gyroSensor.getHeading();
-        if(turnComplete==false) {
+    void turnWithGyro(int degrees) throws InterruptedException {
+        resetGyro();
+        telemetry.addData("heading ", "" + heading());
+        if (degrees < 0)
+            degrees += 370;
 
-            if (degrees > 180) {
-                if (degrees+lastgyro<curDegs) {
-                    FR.setPower(-1);
-                    BR.setPower(-1);
-                    FL.setPower(-1);
-                    BL.setPower(-1);
-                }
-                else turnComplete=true;
-            } else if(degrees+lastgyro>curDegs){
 
-                FR.setPower(1);
-                BR.setPower(1);
-                FL.setPower(1);
-                BL.setPower(1);
+        else
+            degrees-=10;
+
+        if (degrees > 180) {
+            while (degrees < heading() || heading()<10) {
+                telemetry.addData("heading ", "" + heading());
+                FR.setPower(-0.5);
+                BR.setPower(-0.5);
+                FL.setPower(-0.5);
+                BL.setPower(-0.5);
+                sleep(1);
             }
-            else turnComplete=true;
-        }
 
-       if(turnComplete==true){
-           turnComplete=false;
             FL.setPower(0);
             BL.setPower(0);
             FR.setPower(0);
             BR.setPower(0);
-           lastgyro = gyroSensor.getHeading();
-            i++;
+
+        } else {
+            while (degrees > heading() || heading()>350) {
+                telemetry.addData("heading ", "" + heading());
+                FR.setPower(0.5);
+                BR.setPower(0.5);
+                FL.setPower(0.5);
+                BL.setPower(0.5);
+                sleep(1);
+            }
+            FL.setPower(0);
+            BL.setPower(0);
+            FR.setPower(0);
+            BR.setPower(0);
         }
+
+        FL.setPower(0);
+        BL.setPower(0);
+        FR.setPower(0);
+        BR.setPower(0);
+        telemetry.addData("Done","");
+
     }
 
     void pause(float pauseAmount) {
@@ -110,5 +97,13 @@ public class turnTest extends OpMode {
             i++;
         }
         loopCount++;
+    }
+
+    int heading(){
+        int head;
+        head=gyroSensor.getHeading()-lastgyro;
+        if (head<0)
+            head+=360;
+        return (head);
     }
 }
