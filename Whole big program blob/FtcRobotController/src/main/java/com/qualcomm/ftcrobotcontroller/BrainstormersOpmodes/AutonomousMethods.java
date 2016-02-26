5261f9c1e822569ddf8edd90ea3d29f7415734f8
Dan Broun <dnlbroun@gmail.com>
2016-02-26 17:19:48 -0500

    commented autonomous methods

--- Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/BrainstormersOpmodes/AutonomousMethods.java
+++ Whole big program blob/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/BrainstormersOpmodes/AutonomousMethods.java
@@ -3,8 +3,9 @@ package com.qualcomm.ftcrobotcontroller.BrainstormersOpmodes;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 
 /**
- * Created by August on 2/8/2016.
+ * Created by August & Ethan & Dan on 12/1/2015.
  */
+
 public abstract class AutonomousMethods extends AutonomousBuildingBlocks {
     /**
      * uses the gyro to turn
@@ -12,64 +13,75 @@ public abstract class AutonomousMethods extends AutonomousBuildingBlocks {
      * @param degrees target degrees
      * @throws InterruptedException
      */
-    void turnTo(int degrees, int tollerance) throws InterruptedException {
+    void turnTo(int degrees, int tolerance) throws InterruptedException {
         int heading, difference, countwithintolerence = 0, count = 0, cyclesMaxPower = 0;
-        degrees*=turnDirection;
+        degrees *= turnDirection;
         double power;
         boolean rightTurn = false;
         run_using_encoders();
-        while (true) {
+
+        while (true) { //while the turn hasn't been completed we run through this loop
             count++;
             heading = gyroSensor.getHeading();
-            difference = (degrees - heading) % 360;
-            if (difference > 180)
+            difference = (degrees - heading) % 360; //calculates the angle based on where the robot is now and how far it has to go
+
+            if (difference > 180) { //determines which way the robot will turn (left or right)
                 difference = difference - 360;
-            else if (difference < -180)
+            } else if (difference < -180) {
                 difference = 360 + difference;
-            telemetry.addData("Heading", " " + heading + " " + difference + " " + rightTurn);
+            }
+
+            telemetry.addData("Heading", " " + heading + " " + difference + " " + rightTurn); //determines how fast the turn should be, as the turn gets greater the speed gets faster
             power = Math.abs(difference / 45);
             power = clip(power, 0.075, 0.35);
-            if (cyclesMaxPower==0)
-                cyclesMaxPower=Math.abs(difference/30)+3;
-            if (count < cyclesMaxPower && Math.abs(difference)> 20){ //maxs out power to speed up turn during the beginning of the turn
+
+            if (cyclesMaxPower == 0) {
+                cyclesMaxPower = Math.abs(difference / 30) + 3;
+            }
+
+            if (count < cyclesMaxPower && Math.abs(difference) > 20) { //speeds up out power to speed up turn during the beginning of the turn
                 power = 1;
             }
-            if (Math.abs(difference) <= tollerance) {
+
+            if (Math.abs(difference) <= tolerance) { //how far off the turn can be while still being successful (tolerance of turn)
                 countwithintolerence++;
-                if (countwithintolerence > 15)
+                if (countwithintolerence > 15) {
                     break;
+                }
                 setLeftPower(0);
                 setRightPower(0);
             } else if (difference > 0) {
-                countwithintolerence=0;
+                countwithintolerence = 0;
                 setLeftPower(power);
                 setRightPower(-power);
                 rightTurn = true;
             } else {
-                countwithintolerence=0;
+                countwithintolerence = 0;
                 setLeftPower(-power);
                 setRightPower(power);
                 rightTurn = false;
             }
+
             waitOneFullHardwareCycle();
         }
+
         telemetry.addData("Do", "ne");
         setLeftPower(0);
         setRightPower(0);
         sleep(20);
         setLeftPower(0);
         setRightPower(0);
-
     }
 
     /**
      * drives until the ultrasonic sensors read a certain value
+     *
      * @param target target distance
      * @param speed  movement speed
      * @throws InterruptedException
      * @see #readFixedUltra(UltrasonicSensor)
      */
-    void driveUntilUltra(int target, double speed, int maxdistance) throws InterruptedException {
+    void driveUntilUltra(int target, double speed, int maxdistance) throws InterruptedException { //drives until the robot gets within a certain distance of an object
         while (readFixedUltra(ultra2) > target || readFixedUltra(ultra2) < 1) {
             driveForever(speed);
             waitOneFullHardwareCycle();
@@ -77,27 +89,6 @@ public abstract class AutonomousMethods extends AutonomousBuildingBlocks {
         stopMotors();
     }
 
-    void driveForeverWitGyro(double speed) throws InterruptedException {
-        double turnheading;
-        double currSpeed = speed;
-        // telemetry.addData("encoder values", "right:" + FR.getCurrentPosition() + " left:" + FL.getCurrentPosition());
-        turnheading = heading();
-        if (turnheading > 180)
-            turnheading -= 360;
-        turnheading /= 15;
-
-        if (Math.abs(turnheading) > 3)
-            currSpeed = clip(currSpeed, -0.9, 0.9);
-
-        else if (turnheading != 0)
-            currSpeed = clip(currSpeed, -0.95, 0.95);
-
-        telemetry.addData("heading ", "" + heading());
-        telemetry.addData("absolute heading", " " + gyroSensor.getHeading());
-        run_using_encoders();
-        setLeftPower(currSpeed + turnheading / 3);
-        setRightPower(currSpeed - turnheading / 3);
-    }
 
     /**
      * uses encoders, ultrasonic sensors and gyro sensors to drive accurately
@@ -111,45 +102,47 @@ public abstract class AutonomousMethods extends AutonomousBuildingBlocks {
      * @throws InterruptedException
      */
     void drive(float distance, double speed, boolean avoidance, boolean correction, int targetType) throws InterruptedException {
-        int count = 0;
-        int blockcount = 0;
+        int count = 0; //how many times the loop has run
+        int blockedcount = 0; //determines how long the robot has been blocked for
         resetEncoderDelta();
         resetGyro();
+
+        //start the drive wheel motors at full power otherwise when calculating turn heading from encoders all will be null and program will halt
         run_using_encoders();
-        // Start the drive wheel motors at full power
-        setLeftPower(0.5); //otherwise when calculating turn heading from encoders all will be null and program will halt
+        setLeftPower(0.5);
         setRightPower(0.5);
+
         sleep(20);
-        boolean isComplete=false;
+        boolean isComplete = false;
         do {
-            count ++;
+            count++;
             telemetry.addData("drive", "working");
             double turnheading = 0;
             double currSpeed = speed;
-            // telemetry.addData("encoder values", "right:" + FR.getCurrentPosition() + " left:" + FL.getCurrentPosition());
-            if (correction == true) {
+
+            if (correction == true) { //using encoder values to correct offsets/drift
                 turnheading += (
                         FLposition() +
                                 BLposition() -
                                 FRposition() -
                                 BRposition()
-                ) / 80.0; //must be float
+                ) / 80.0; //MUST BE FLOAT
 
-                //ramping up speed so it doesn't jerk
+                //ramping up speed to avoid jerk
                 if (Math.abs(turnheading) > 0.5 || count < 50) {
                     currSpeed = clip(currSpeed, -0.6, 0.6);
                 }
 
-                if (blocked()  &&  speed > 0 && avoidance) {
-                    blockcount ++;
-                    if (blockcount > 3) {
+                if (blocked() && speed > 0 && avoidance) { //if the robot is blocked, it will stop
+                    blockedcount++;
+                    if (blockedcount > 3) {
                         currSpeed = 0;
                         telemetry.addData("Blocked", "Blocked!!!!");
                     }
+                } else {
+                    blockedcount = 0; //resets block count after the robot is no longer blocked
                 }
-                else {
-                    blockcount = 0;
-                }
+
                 if (turnheading != 0) {
                     currSpeed = clip(currSpeed, -0.95, 0.95);
                 }
@@ -160,49 +153,44 @@ public abstract class AutonomousMethods extends AutonomousBuildingBlocks {
             } else {
                 turnheading = 0;
             }
-            turnheading=clip(turnheading, -0.25, 0.25);
+
+
+            turnheading = clip(turnheading, -0.25, 0.25); //clips the turn heading
+
+            //sets the power (repeated to avoid glitching)
             telemetry.addData("encoder values", " FL " + FLposition() + " BL " + BLposition() + " FR " + FRposition() + " BR " + BRposition());
             run_using_encoders();
             setLeftPower(currSpeed + turnheading * currSpeed);
             setRightPower(currSpeed - turnheading * currSpeed);
-            telemetry.addData("left power ", + (currSpeed + turnheading*currSpeed) + " right power: " + (currSpeed - turnheading * currSpeed));
-            telemetry.addData("encoder values", " FL " + FLposition()+ " " +FL.getCurrentPosition() + " BL " + BLposition()+ " " +BL.getCurrentPosition() + " FR " + FRposition() + " " +FR.getCurrentPosition() + " BR " + BRposition()+ " " +BR.getCurrentPosition() );
+            telemetry.addData("left power ", +(currSpeed + turnheading * currSpeed) + " right power: " + (currSpeed - turnheading * currSpeed));
+            telemetry.addData("encoder values", " FL " + FLposition() + " " + FL.getCurrentPosition() + " BL " + BLposition() + " " + BL.getCurrentPosition() + " FR " + FRposition() + " " + FR.getCurrentPosition() + " BR " + BRposition() + " " + BR.getCurrentPosition());
             run_using_encoders();
-            turnheading = clip(turnheading, -1,1);
-            setLeftPower(currSpeed +  turnheading * (currSpeed));
+            turnheading = clip(turnheading, -1, 1);
+            setLeftPower(currSpeed + turnheading * (currSpeed));
             setRightPower(currSpeed - turnheading * (currSpeed));
             telemetry.addData("left power ", +(currSpeed + turnheading * currSpeed) + " right power: " + (currSpeed - turnheading * currSpeed));
+
             waitOneFullHardwareCycle();
 
-            switch (targetType){
+            switch (targetType) {  //determines whether or not to stop when the color sensor detects white
                 case 0:
-                    isComplete=(hasLeftReached(distance)||hasRightReached(distance));
+                    isComplete = (hasLeftReached(distance) || hasRightReached(distance));
                     break;
                 case 1:
-                    isComplete=colorSensor2.alpha()>1||hasLeftReached(distance)||hasRightReached(distance);
+                    isComplete = colorSensor2.alpha() > 1 || hasLeftReached(distance) || hasRightReached(distance);
                     telemetry.addData("alpha", colorSensor2.alpha());
                     break;
                 default:
                     telemetry.addData("Invalid input", "stopping");
-                    isComplete=true;
+                    isComplete = true;
                     break;
             }
 
-        } while (isComplete==false);
+        }
+
+        while (isComplete == false); //ends the drive function
         telemetry.addData("drive", "complete");
         stopMotors();
         sleep(50);
-    }
-
-    /**
-     * runs {@link #drive(float, double, boolean, boolean, int)} without needing to input
-     * collision avoidance or course correction
-     * @param distance distance to travel in encoder clicks
-     * @param speed    speed to travel at
-     * @throws InterruptedException
-     */
-    //Compressed the two drives into one for simplicity - "Ethan ;)"
-    void drive(float distance, double speed) throws InterruptedException {
-        drive(distance, speed, true, true, 0);
     }
 }
