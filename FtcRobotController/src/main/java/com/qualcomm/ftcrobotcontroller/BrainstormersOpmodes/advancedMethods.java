3c0c4bf24c7b956ec2963a7778f8d9a6a3a88ce3
ickdinner <ethan.oliver@gmail.com>
2016-03-12 14:41:32 -0500

    Added turnto to advanced methods

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/BrainstormersOpmodes/advancedMethods.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/BrainstormersOpmodes/advancedMethods.java
@@ -14,4 +14,68 @@ public abstract class advancedMethods extends AutonomousMethods {
 
         }
     }
+
+    void turnTo(int degrees, int tolerance) throws InterruptedException {
+        int  countwithintolerence = 0, count = 0;
+        double heading,difference, cyclesMaxPower = 0;
+
+        degrees *= turnDirection;
+        double power;
+        boolean rightTurn = false;
+        run_using_encoders();
+
+        while (true) { //while the turn hasn't been completed we run through this loop
+            count++;
+            heading = gyroSensor.getHeading();
+            difference = (degrees - heading) % 360; //calculates the angle based on where the robot is now and how far it has to go
+
+            if (difference > 180) { //determines which way the robot will turn (left or right)
+                difference = difference - 360;
+            } else if (difference < -180) {
+                difference = 360 + difference;
+            }
+
+            telemetry.addData("Heading", " " + heading + " " + difference + " " + rightTurn); //determines how fast the turn should be, as the turn gets greater the speed gets faster
+            power = Math.abs(difference / 45);
+            power = clip(power, 0.075, 0.35);
+
+            if (cyclesMaxPower == 0) {
+                cyclesMaxPower = Math.abs(difference / 30) + 3;
+            }
+
+            if (count < cyclesMaxPower && Math.abs(difference) > 20) { //speeds up out power to speed up turn during the beginning of the turn
+                power = 1;
+            }
+
+            if (Math.abs(difference) <= tolerance) { //how far off the turn can be while still being successful (tolerance of turn)
+                countwithintolerence++;
+                if (countwithintolerence > 15) {
+                    break;
+                }
+                setLeftPower(0);
+                setRightPower(0);
+            } else if (difference > 0) {
+                countwithintolerence = 0;
+                setLeftPower(power);
+                setRightPower(-power);
+                rightTurn = true;
+            } else {
+                countwithintolerence = 0;
+                setLeftPower(-power);
+                setRightPower(power);
+                rightTurn = false;
+            }
+
+            waitOneFullHardwareCycle();
+        }
+
+        telemetry.addData("Do", "ne");
+        setLeftPower(0);
+        setRightPower(0);
+        sleep(20);
+        setLeftPower(0);
+        setRightPower(0);
+    }
+
+
 }
